519b118ab212b6b99a71c94814fd84eecd49ae1b
kumabiko <nousbear.dev@gmail.com>
2023-04-23 12:48:56 +0900

    ✨ Simple Component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,22 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-a11y",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
   features: {
-    interactionsDebugger: true,
+    postcss: false,
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
